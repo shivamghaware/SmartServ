9b556223b3813961618d50dbdf471f0c1f363619
shivamghaware <shivamghaware3@gmail.com>
2026-07-04 18:29:26 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/smartserv/SmartservApplication.java
@@ -0,0 +1,13 @@
+package com.smartserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartservApplication.class, args);
+	}
+
+}
